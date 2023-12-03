3aa5eef0c7d36f01438476ff014754feba697ebf
Andrey B <aatuline@gmail.com>
2023-12-03 17:12:13 +0400

    fix test

--- tests/Integration/Migration/CustomerMigrationTest.php
+++ tests/Integration/Migration/CustomerMigrationTest.php
@@ -43,7 +43,7 @@ class CustomerMigrationTest extends KernelTestCase
             static::class,
             1,
             10,
-            2,
+            1,
         );
 
         $migration = new Migration(
